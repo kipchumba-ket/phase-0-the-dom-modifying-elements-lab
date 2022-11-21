aa2dc9f10ddf33ab0875195a8a346440581ea7c9
kipchumba-ket <kipchumbaelly97@gmail.com>
2022-11-21 09:50:31 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+function removeMain() {
+    const main = document.getElementById("main");
+    main.remove();
+  }
+  
+  removeMain();
+  
+  const newHeader = document.createElement("h1");
+  newHeader.id = "victory";
+  
+  newHeader.innerHTML = "ELLY is the champion";
